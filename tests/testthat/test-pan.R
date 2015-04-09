b9e2c60e7f535aabf8f11c4919f122c91cf3c11f
edisz <eduardszoecs@gmail.com>
2015-04-09 22:02:20 +0200

    fixed test for windows?!

--- tests/testthat/test-pan.R
+++ tests/testthat/test-pan.R
@@ -4,6 +4,6 @@ test_that("pan()", {
   expect_error(pan(c('xxxxx', 'aaaaaaa')))
   expect_equal(pan('xxxxx', verbose = FALSE), NA)
   expect_equal(length(pan('2,4-dichlorophenol', verbose = FALSE)), 73)
-  expect_equal(pan('Triclosan', verbose = FALSE)$`CAS Number`, "3380-34-5")
+  expect_equal(pan('Triclosan', verbose = FALSE)[[2]], "3380-34-5")
   expect_equal(length(pan('Chlorpyrifos', verbose = FALSE, first = TRUE)[[1]]), 1)
 })
