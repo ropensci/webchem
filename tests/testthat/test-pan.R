c0d3133e11ac37f02940a15a2a75b8e7dd32f8ce
edisz <eduardszoecs@gmail.com>
2015-04-13 16:57:24 +0200

    tests

--- tests/testthat/test-pan.R
+++ tests/testthat/test-pan.R
@@ -2,6 +2,7 @@ context("pan")
 
 test_that("pan()", {
   expect_error(pan(c('xxxxx', 'aaaaaaa')))
+  expect_warning(pan(NA))
   expect_equal(pan('xxxxx', verbose = FALSE), NA)
   expect_equal(length(pan('2,4-dichlorophenol', verbose = FALSE)), 73)
   expect_equal(pan('Triclosan', verbose = FALSE)[[2]], "3380-34-5")
