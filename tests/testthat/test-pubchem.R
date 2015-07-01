1c4d620521c5068969976eaf93dcfd00ca2f7ae5
edisz <eduardszoecs@gmail.com>
2015-07-01 20:21:39 +0200

    fixed checks for pubchem

--- tests/testthat/test-pubchem.R
+++ tests/testthat/test-pubchem.R
@@ -1,7 +1,7 @@
 context("pubchem")
 
 test_that("get_cid()", {
-  expect_equal(get_cid('Triclosan')[1], '4093', verbose = FALSE)
+  expect_equal(get_cid('Triclosan')[1], '5564', verbose = FALSE)
   expect_equal(length(get_cid('Triclosan', first = TRUE, verbose = FALSE)), 1)
   expect_equal(get_cid('xxxxx', verbose = FALSE), NA)
   expect_error(get_cid(c('xxxxx', 'aaaaaaa')))
@@ -11,8 +11,8 @@ test_that("get_cid()", {
 
 test_that("cid_compinfo", {
   expect_error(cid_compinfo(c('xxxxx', 'aaaaaaa')))
-  expect_equal(cid_compinfo('4093', verbose = FALSE)$CanonicalSmiles, "CC(=C)C(=O)O")
-  expect_equal(length(cid_compinfo('4093', verbose = FALSE)), 25)
-  expect_equal(length(cid_compinfo('4093', first = TRUE, verbose = FALSE)$synonyms), 1)
+  expect_equal(cid_compinfo('5564', verbose = FALSE)$CanonicalSmiles, "CC(=C)C(=O)O")
+  expect_equal(length(cid_compinfo('5564', verbose = FALSE)), 25)
+  expect_equal(length(cid_compinfo('5564', first = TRUE, verbose = FALSE)$synonyms), 1)
   expect_equal(get_cid('xxxxx', verbose = FALSE), NA)
 })
