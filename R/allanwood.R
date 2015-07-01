7ecdccfb65388027e852b1da71c00e0ac8cbd8a0
edisz <eduardszoecs@gmail.com>
2015-07-01 21:45:48 +0200

    added cas search to allanwood

--- R/allanwood.R
+++ R/allanwood.R
@@ -6,15 +6,16 @@
 #' @param  x character; search string
 #' @param type character; type of input
 #' @param verbose logical; print message during processing to console?
-#' @return A list of eight entries: status, preferredd IUPAC Name, IUPAC Name,
-#'          cas, formula, activity, inchikey, inchi
+#' @return A list of eight entries: common-name, status, preferredd IUPAC Name,
+#'          IUPAC Name, cas, formula, activity, inchikey, inchi
 #'
 #' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
 #' @export
 #' @examples
 #' \dontrun{
 #' allanwood('Fluazinam', type = 'commonname')
-#' sapply(c('Fluazinam', 'Diclofop', 'xxxxx'), allanwood)
+#' sapply(c('Fluazinam', 'Diclofop', 'xxxxx'), allanwood, type = 'com')
+#' allanwood("79622-59-6", type = 'cas')
 #' }
 allanwood <- function(x, type = c("commonname", "cas"), verbose = TRUE){
   type <- match.arg(type)
@@ -24,16 +25,41 @@ allanwood <- function(x, type = c("commonname", "cas"), verbose = TRUE){
     names <- xpathSApply(ttt, "//a" , xmlValue)
     names <- names[!names == '']
     links <- xpathSApply(ttt,"//a//@href")
-    takelink <- links[tolower(names) == tolower(x)]
-  } else {
-    stop('currently not implemented!')
+    cname <-  x
   }
+  if (type == 'cas') {
+    baseurl0 <- 'http://www.alanwood.net/pesticides/index_rn.html'
+    ttt0 <- htmlParse(getURL(baseurl0))
+    names0 <- xpathSApply(ttt0, "//dl/dt" , xmlValue)
+    # select only first link
+    links0 <- xpathSApply(ttt0, '//dt/following-sibling::dd[1]/a[1]/@href')
+    linkn0 <- xpathSApply(ttt0, '//dt/following-sibling::dd[1]/a[1]', xmlValue)
+
+    baseurl1 <- 'http://www.alanwood.net/pesticides/index_rn1.html'
+    ttt1 <- htmlParse(getURL(baseurl1))
+    names1 <- xpathSApply(ttt1, "//dt" , xmlValue)
+    links1 <- xpathSApply(ttt1, '//dt/following-sibling::dd[1]/a[1]/@href')
+    linkn1 <- xpathSApply(ttt1, '//dt/following-sibling::dd[1]/a[1]', xmlValue)
+
+    baseurl2 <- 'http://www.alanwood.net/pesticides/index_rn2.html'
+    ttt2 <- htmlParse(getURL(baseurl2))
+    names2 <- xpathSApply(ttt2, "//dt" , xmlValue)
+    links2 <- xpathSApply(ttt2, '//dt/following-sibling::dd[1]/a[1]/@href')
+    linkn2 <- xpathSApply(ttt2, '//dt/following-sibling::dd[1]/a[1]', xmlValue)
+
+    names <- c(names0, names1, names2)
+    links <- c(links0, links1, links2)
+    linknames <- c(linkn0, linkn1, linkn2)
+    cname <-  linknames[tolower(names) == tolower(x)]
+  }
+  takelink <- links[tolower(names) == tolower(x)]
   if (length(takelink) == 0) {
     message('Not found! Returning NA.\n')
     return(NA)
   }
   if (verbose)
     message('Querying ', takelink)
+
   Sys.sleep(0.3)
   ttt <- htmlParse(getURL(paste0('http://www.alanwood.net/pesticides/', takelink)))
   status <- xpathSApply(ttt, "//tr/th[@id='r1']/following-sibling::td", xmlValue)
@@ -44,7 +70,7 @@ allanwood <- function(x, type = c("commonname", "cas"), verbose = TRUE){
   activity <- xpathSApply(ttt, "//tr/th[@id='r7']/following-sibling::td", xmlValue)
   inchikey <- xpathSApply(ttt, "//tr/th[@id='r11']/following-sibling::td", xmlValue)
   inchi <- xpathSApply(ttt, "//tr/th[@id='r12']/following-sibling::td", xmlValue)
-  out <- list(status = status, pref_iupac_name = pref_iupac_name,
+  out <- list(cname = cname, status = status, pref_iupac_name = pref_iupac_name,
               iupac_name = iupac_name, cas = cas, formula = formula,
               activity = activity, inchikey = inchikey, inch = inchi)
   return(out)
--- tests/testthat/test-allanwood.R
+++ tests/testthat/test-allanwood.R
@@ -6,5 +6,14 @@ xx <- allanwood('xxxxx', type = 'commonname')
 test_that("allanwood, commonname", {
   expect_equal(fl$cas, "79622-59-6", verbose = FALSE)
   expect_equal(xx, NA, verbose = FALSE)
-  expect_equal(length(fl), 8)
+  expect_equal(length(fl), 9)
+})
+
+
+cs <-  allanwood("79622-59-6", type = 'cas')
+test_that("allanwood, cas", {
+  expect_equal(cs$cas, "79622-59-6", verbose = FALSE)
+  expect_equal(cs$cname, "fluazinam", verbose = FALSE)
+  expect_equal(xx, NA, verbose = FALSE)
+  expect_equal(length(fl), 9)
 })
