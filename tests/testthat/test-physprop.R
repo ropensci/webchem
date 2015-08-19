dae3264015818e1fb56c91580de4a53b956106c6
edisz <eduardszoecs@gmail.com>
2015-08-19 18:05:00 +0200

    fixed tests

--- tests/testthat/test-physprop.R
+++ tests/testthat/test-physprop.R
@@ -3,8 +3,8 @@ context("physprop")
 fl <- physprop('50-00-0')
 xx <- physprop('xxxxx')
 
-test_that("physprop return correct resutls", {
-  exepct_equal(length(fl), 4)
+test_that("physprop returns correct results", {
+  expect_equal(length(fl), 4)
   expect_equal(fl$cas, "50-00-0")
   expect_equal(xx, NA)
   expect_equal(names(fl$prop), c("variable", "value", "unit", "temp", "type", "ref"))
