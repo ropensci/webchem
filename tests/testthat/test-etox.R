c2f4478016f4451cb37a09bae1841682a3b3e12f
edisz <eduardszoecs@gmail.com>
2015-08-21 12:07:14 +0200

    more tests for etox

--- tests/testthat/test-etox.R
+++ tests/testthat/test-etox.R
@@ -1,44 +1,50 @@
 context("etox")
 
-do <- get_etoxid('Triclosan')
-xx <- get_etoxid('xxxxx')
-
 test_that("get_etoxid returns correct results", {
+  do <- get_etoxid('Triclosan')
+  xx <- get_etoxid('xxxxx')
+
   expect_equal(c(do), "20179")
   expect_equal(attr(do, "matched"), "Triclosan ( 20179 )")
   expect_equal(c(xx), NA)
 })
 
-do2 <- etox_basic('20179')
-xx2 <- etox_basic('xxx')
 
 test_that("etox_basic returns correct results", {
+  do2 <- etox_basic('20179')
+  xx2 <- etox_basic('xxx')
+
   expect_equal(do2$cas, "3380-34-5")
   expect_equal(length(do2), 4)
   expect_is(do2$synonyms, 'data.frame')
   expect_equal(xx2, NA)
 })
 
-do3 <- etox_targets('20179')
-xx3 <- etox_targets('xxxx')
 
-test_that("etox_basic returns correct results", {
-  expect_equal(do3$Stoff[1], "Triclosan")
+test_that("etox_targets returns correct results", {
+  do3 <- etox_targets('20179')
+  xx3 <- etox_targets('xxxx')
+
+  expect_equal(do3$Substance[1], "Triclosan")
   expect_equal(ncol(do3), 32)
   expect_is(do3, 'data.frame')
   expect_equal(xx3, NA)
 })
 
-int1 <- etox_basic(do)
-int2 <- etox_targets(do)
-int3 <- etox_basic(xx)
-int4 <- etox_targets(xx)
+
 test_that("etox integration tests", {
+  do <- get_etoxid('Triclosan')
+  xx <- get_etoxid('xxxxx')
+  int1 <- etox_basic(do)
+  int2 <- etox_targets(do)
+  int3 <- etox_basic(xx)
+  int4 <- etox_targets(xx)
+
   expect_equal(int1$cas, "3380-34-5")
   expect_equal(length(int1), 4)
   expect_is(int1$synonyms, 'data.frame')
 
-  expect_equal(int2$Stoff[1], "Triclosan")
+  expect_equal(int2$Substance[1], "Triclosan")
   expect_equal(ncol(int2), 32)
   expect_is(int2, 'data.frame')
 
