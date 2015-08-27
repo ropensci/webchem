2d9d39de90519d666c8432cd0eb46441399a20e2
edisz <eduardszoecs@gmail.com>
2015-08-27 10:47:32 +0200

    more fixes to tests?

--- tests/testthat/test-pubchem.R
+++ tests/testthat/test-pubchem.R
@@ -14,6 +14,7 @@ test_that("get_cid()", {
 
   expect_equal(get_cid('Triclosan')[1], '5564')
   expect_equal(length(get_cid('Triclosan', first = TRUE, verbose = FALSE)), 1)
+  Sys.sleep(0.2)
   expect_equal(get_cid('xxxxx', verbose = FALSE), NA)
   expect_error(get_cid(c('xxxxx', 'aaaaaaa')))
   expect_warning(get_cid(NA))
@@ -24,6 +25,7 @@ test_that("cid_compinfo", {
   chk_pubchem()
 
   expect_error(cid_compinfo(c('xxxxx', 'aaaaaaa')))
+  Sys.sleep(0.2)
   expect_equal(cid_compinfo('5564', verbose = FALSE)$CanonicalSmiles, "C1=CC(=C(C=C1Cl)O)OC2=C(C=C(C=C2)Cl)Cl")
   expect_equal(length(cid_compinfo('5564', verbose = FALSE)), 25)
   expect_equal(length(cid_compinfo('5564', first = TRUE, verbose = FALSE)$synonyms), 1)
