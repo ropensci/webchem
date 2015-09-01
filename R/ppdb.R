19dae8949150e4fa38f69714b61cdee27b78044d
edisz <eduardszoecs@gmail.com>
2015-09-01 14:09:46 +0200

    make ppdf more general

--- R/ppdb.R
+++ R/ppdb.R
@@ -91,6 +91,12 @@ ppdb_buildidx <- function(){
 ppdb_query <- function(cas, verbose = TRUE){
   # cas <- '1071-83-6'
   # cas <- '50-00-0' # currently fails!
+
+  chk <- function(x){
+    if (inherits(x, 'try-error'))
+      return(NA)
+    return(x)
+  }
   qurl <- ppdb_idx[ppdb_idx$cas == cas, 'link']
   if (length(qurl) == 0) {
     message('CAS not found! Returning NA.\n')
@@ -100,26 +106,31 @@ ppdb_query <- function(cas, verbose = TRUE){
     message('Querying ', qurl)
 
   tt <- getURL(qurl)
-  tables <- readHTMLTable(tt, stringsAsFactors = FALSE, header = FALSE)
   ttt <- htmlParse(tt)
 
   # ec regulation
-  ec_regulation <- tables[[5]]
-  colnames(ec_regulation) <- ec_regulation[1, ]
-  ec_regulation <- ec_regulation[-1, ]
+  ec_regulation <- readHTMLTable(getNodeSet(ttt, "//p[contains(.,'EC Reg')]/following-sibling::table[1]")[[1]],
+                                 stringsAsFactors = FALSE)
+
 
   # approved in contries
   status <- unlist(xpathApply(ttt, "//*[contains(.,'Approved')]/following-sibling::table[1]/tr[2]/td/p/img/@src"))
-  approved_id <- data.frame(t(tables[[6]]))
+  approved_id <- data.frame(t(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'Approved for use')]/following-sibling::table[1]")[[1]],
+                                            stringsAsFactors = FALSE)))
   approved_id$status <- ifelse(status == 'tick.jpg', TRUE, FALSE)
-  approved_id[,'X2'] <- NULL
-  names(approved_id)[1] <- 'country'
+  approved_id[ , 1] <- NULL
+  approved_id$county <- rownames(approved_id)
 
   # general status
-  general <- tables[[7]]
+  general <- readHTMLTable(getNodeSet(ttt, "//p[contains(.,'General status')]/following-sibling::table[1]")[[1]],
+                           stringsAsFactors = FALSE)
   names(general) <- c('variable', 'value')
 
   # parents
+  parents <- try(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'Can be a metabolite of')]/following-sibling::table[1]")[[1]],
+                               stringsAsFactors = FALSE),
+                 silent = TRUE)
+  parents <- chk(parents)
 
 
 #   # formulations
@@ -127,75 +138,103 @@ ppdb_query <- function(cas, verbose = TRUE){
 #   names(formulation) <- c('variable', 'value')
 
   # fate
-  fate <- tables[[9]]
-  take <- fate[!is.na(fate$V5), ]
-  take[ , 'V1'] <- paste(take[ , 'V1'], take[ , 'V2'])
-  take[ , 'V2'] <- NULL
-  fate <- fate[is.na(fate$V5), 1:4]
-  nam <- fate[1 , ]
-  fate <- fate[-1, ]
-  colnames(fate) <- nam
-  colnames(take) <- nam
-  fate <- rbind(fate, take)
-  fate <- fate[!grepl('Note', fate[ , 'Property']), ]
-  take <- fate[is.na(fate[ , 'Interpretation']), ]
-  corr <- which(is.na(fate[ , 'Interpretation']))[1] - 1
-  take <- data.frame(Prop = NA, take[ , c(1,2,3)])
-  take[ , 1] <- fate[corr, 'Property']
-  colnames(take) <- nam
-  fate <- fate[!is.na(fate[ , 'Interpretation']), ]
-  fate <- rbind(fate, take)
+  fate <- try(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'ENVIRONMENTAL FATE')]/following-sibling::table[1]")[[1]],
+                            stringsAsFactors = FALSE),
+                 silent = TRUE)
+  fate <- chk(fate)
+  if (!length(fate) == 0) {
+    take <- fate[!is.na(fate[ , 'V5']), ]
+    take[ , 'V1'] <- paste(take[ , 'V1'], take[ , 'V2'])
+    take[ , 'V2'] <- NULL
+    fate <- fate[is.na(fate[ , 'V5']), 1:4]
+    nam <- c('Property', 'Value', 'Source/Quality', 'Interpretation')
+    colnames(fate) <- nam
+    colnames(take) <- nam
+    fate <- rbind(fate, take)
+    fate <- fate[!grepl('Note', fate[ , 'Property']), ]
+    take <- fate[is.na(fate[ , 'Interpretation']), ]
+    corr <- which(is.na(fate[ , 'Interpretation']))[1] - 1
+    take <- data.frame(Prop = NA, take[ , c(1,2,3)])
+    take[ , 1] <- fate[corr, 'Property']
+    colnames(take) <- nam
+    fate <- fate[!is.na(fate[ , 'Interpretation']), ]
+    fate <- rbind(fate, take)
+  }
+
 
   # degredation
-  deg <- tables[[10]]
-  colnames(deg) <- deg[1, ]
-  deg <- deg[-1, ]
-  deg <- deg[!grepl('Note', deg[ , 'Property']), ]
+  deg <-  try(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'Degradation')]/following-sibling::table[1]")[[1]],
+                            stringsAsFactors = FALSE),
+              silent = TRUE)
+  deg <- chk(deg)
+  if (!length(deg) == 0) {
+    nam <- c('Property', 'Value', 'Source/Quality', 'Interpretation')
+    colnames(deg) <- nam
+    deg <- deg[!grepl('Note', deg[ , 'Property']), ]
 
-  take <- deg[deg[ , 'Value'] == 'Value', ]
-  take <- data.frame(take[ , -2], N = NA)
-  colnames(take) <- colnames(deg)
-  deg <- deg[!deg[ , 'Value'] == 'Value', ]
-  deg <- rbind(deg, take)
+    take <- deg[deg[ , 'Value'] == 'Value', ]
+    take <- data.frame(take[ , -2], N = NA)
+    colnames(take) <- colnames(deg)
+    deg <- deg[!deg[ , 'Value'] == 'Value', ]
+    deg <- rbind(deg, take)
 
-  corr <- deg[!is.na(deg[,5]), 1]
-  deg[!is.na(deg[,5]), 1] <- paste(deg[!is.na(deg[,5]), 1], deg[!is.na(deg[,5]), 2])
-  deg[!is.na(deg[,5]), ] <- c(deg[!is.na(deg[,5]), c(1, 3, 4, 5)], NA)
-  deg[ , 5] <- NULL
+    corr <- deg[!is.na(deg[,5]), 1]
+    deg[!is.na(deg[ , 5]), 1] <- paste(deg[!is.na(deg[ , 5]), 1], deg[!is.na(deg[ , 5]), 2])
+    deg[!is.na(deg[ , 5]), ] <- c(deg[!is.na(deg[ , 5]), c(1, 3, 4, 5)], NA)
+    deg[ , 5] <- NULL
+  }
+
+
 
   # soil adsorption and mobility
-  soil <- tables[[11]]
-  colnames(soil) <- soil[1, ]
-  soil <- soil[-1, ]
-  soil <- soil[!grepl('Note', soil[ , 'Property']), ]
-  corr <- soil[!is.na(soil[,5]), 1]
-  soil[!is.na(soil[ , 5]), 1] <- paste(soil[!is.na(soil[ , 5]), 1], soil[!is.na(soil[ , 5]), 2])
-  soil[!is.na(soil[ , 5]), ] <- c(soil[!is.na(soil[ , 5]), c(1, 3, 4, 5)], NA)
-  soil[ , 5] <- NULL
+  soil <- try(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'Soil adsorption')]/following-sibling::table[1]")[[1]],
+                            stringsAsFactors = FALSE),
+              silent = TRUE)
+  soil <- chk(soil)
+  if (!length(soil) == 0) {
+    nam <- c('Property', 'Value', 'Source/Quality', 'Interpretation')
+    colnames(soil) <- nam
+    soil <- soil[!grepl('Note', soil[ , 'Property']), ]
+    corr <- soil[!is.na(soil[,5]), 1]
+    soil[!is.na(soil[ , 5]), 1] <- paste(soil[!is.na(soil[ , 5]), 1], soil[!is.na(soil[ , 5]), 2])
+    soil[!is.na(soil[ , 5]), ] <- c(soil[!is.na(soil[ , 5]), c(1, 3, 4, 5)], NA)
+    soil[ , 5] <- NULL
+  }
+
 
   # metabolites
-  metab <- tables[[12]]
-  colnames(metab) <- metab[1, ]
-  metab <- metab[-1, ]
+  metab <- try(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'Key metabolites')]/following-sibling::table[1]")[[1]],
+                             stringsAsFactors = FALSE),
+               silent = TRUE)
+  metab <- chk(metab)
 
   # ecotoxicology
-  etox <- tables[[13]]
-  colnames(etox) <- etox[1, ]
-  etox <- etox[-1, ]
-  etox <- etox[!grepl('Note', etox[ , 'Property']), ]
-  corr <- etox[!is.na(etox[ , 5]), 1]
-  etox[!is.na(etox[ , 5]), 1] <- paste(etox[!is.na(etox[ , 5]), 1], etox[!is.na(etox[ , 5]), 2])
-  etox[!is.na(etox[ , 5]), ] <- c(etox[!is.na(etox[ , 5]), c(1, 3, 4, 5)], NA)
-  etox[ , 5] <- NULL
+  etox <- try(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'ECOTOXICOLOGY')]/following-sibling::table[1]")[[1]],
+                            stringsAsFactors = FALSE),
+              silent = TRUE)
+  etox <- chk(etox)
+  if (!length(etox) == 0) {
+    nam <- c('Property', 'Value', 'Source/Quality', 'Interpretation')
+    colnames(etox) <- nam
+    etox <- etox[!grepl('Note', etox[ , 'Property']), ]
+    corr <- etox[!is.na(etox[ , 5]), 1]
+    etox[!is.na(etox[ , 5]), 1] <- paste(etox[!is.na(etox[ , 5]), 1], etox[!is.na(etox[ , 5]), 2])
+    etox[!is.na(etox[ , 5]), ] <- c(etox[!is.na(etox[ , 5]), c(1, 3, 4, 5)], NA)
+    etox[ , 5] <- NULL
+  }
+
+
 
   # names
-  names <- tables[[17]]
-  colnames(names) <- names[1, ]
-  names <- names[-1, ]
+  names <- try(readHTMLTable(getNodeSet(ttt, "//p[contains(.,'TRANSLATIONS ')]/following-sibling::table[1]")[[1]],
+                             stringsAsFactors = FALSE),
+               silent = TRUE)
+  names <- chk(names)
 
   out <- list(ec_regulation = ec_regulation,
        approved_in = approved_id,
        general = general,
+       parents = parents,
        # formulation = formulation,
        fate = fate,
        deg = deg,
--- tests/testthat/test-ppdb.R
+++ tests/testthat/test-ppdb.R
@@ -9,8 +9,10 @@ test_that("ppdb_buildidx", {
 
 test_that("ppdb_query", {
   y <- ppdb_query('1071-83-6')
+  z <- ppdb_query('50-00-0')
   xx <- ppdb_query('xxx')
 
   expect_is(y, 'list')
+  expect_is(z, 'list')
   expect_true(is.na(xx))
 })
