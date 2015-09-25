845b166f7115e75add7a91bb351e3ddddfd11d3c
edisz <eduardszoecs@gmail.com>
2015-09-25 10:58:14 +0200

    try another for tests?

--- tests/testthat/test-cir.R
+++ tests/testthat/test-cir.R
@@ -2,7 +2,7 @@ context("cir")
 
 chk_cir <- function(){
   qurl <- 'http://cactus.nci.nih.gov/chemical/structure/Triclosan/cas/xml'
-  Sys.sleep(0.2)
+  Sys.sleep(1)
   cont <- try(getURL(qurl, .encoding = 'UTF-8', .opts = list(timeout = 3)),
               silent = TRUE)
   if (inherits(cont, 'try-error'))
@@ -16,8 +16,10 @@ test_that("cir()", {
   expect_error(cir_query(c('Triclosan', 'Benzol'), 'mw'))
   expect_equal(cir_query('xxxxxxx', 'mw', verbose = FALSE), NA)
   expect_warning(cir_query(NA, 'mw', verbose = FALSE))
-  Sys.sleep(0.2)
+  chk_cir()
   expect_equal(cir_query("3380-34-5", 'stdinchikey', resolver = 'cas_number', verbose = FALSE), "InChIKey=XEFQLINVKFYRCS-UHFFFAOYSA-N")
+  chk_cir()
   expect_true(length(cir_query('Triclosan', 'cas', verbose = FALSE)) > 1)
+  chk_cir()
   expect_equal(length(cir_query('Triclosan', 'cas', first = TRUE, verbose = FALSE)), 1)
 })
