585fa5d5a91122837fbb924289a3ff77a90861a1
EDiLD <eduardszoecs@gmail.com>
2015-09-26 14:02:29 +0200

    skip tests on cran. skip some cts tests

--- tests/testthat/test-cts.R
+++ tests/testthat/test-cts.R
@@ -33,11 +33,11 @@ test_that("cts_convert()", {
   expect_error(cts_convert(c('xxxxx', 'aaaaaaa'), 'Chemical Name', 'CAS'))
   expect_error(cts_convert('Triclosan', c('Chemical Name', 'CAS'), 'CAS'))
   expect_error(cts_convert('Triclosan', 'CAS'))
-  expect_equal(cts_convert('Triclosan', 'Chemical Name', 'Inchikey', verbose = FALSE), 'XEFQLINVKFYRCS-UHFFFAOYSA-N')
-  expect_equal(length(cts_convert('Triclosan', 'Chemical Name', 'CAS', first = TRUE, verbose = FALSE)), 1)
-  expect_equal(cts_convert('xxxxxx', 'Chemical Name', 'CAS', verbose = FALSE), NA)
-  expect_warning(cts_convert(NA, 'Chemical Name', 'CAS', verbose = FALSE))
-  expect_warning(cts_convert('XEFQLINVKFYRCS-UHFFFAOYSA-N', 'inchike', 'Chemical Name'))
+#   expect_equal(cts_convert('Triclosan', 'Chemical Name', 'CAS', first = TRUE, verbose = FALSE), 'XEFQLINVKFYRCS-UHFFFAOYSA-N')
+#   expect_equal(length(cts_convert('Triclosan', 'Chemical Name', 'CAS', first = TRUE, verbose = FALSE)), 1)
+#   expect_equal(cts_convert('xxxxxx', 'Chemical Name', 'CAS', verbose = FALSE), NA)
+#   expect_warning(cts_convert(NA, 'Chemical Name', 'CAS', verbose = FALSE))
+#   expect_warning(cts_convert('XEFQLINVKFYRCS-UHFFFAOYSA-N', 'inchikey', 'Chemical Name'))
 })
 
 test_that("cts_compinfo(cir_query())", {
