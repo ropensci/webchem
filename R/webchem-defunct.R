faaecce5c6651726c864d4ecc22ea18d5eadd2b8
edisz <eduardszoecs@gmail.com>
2015-11-10 08:54:44 +0100

    allanwood is now defunct

---
+++ R/webchem-defunct.R
@@ -0,0 +1,16 @@
+#' Defunct function(s) in the webchem package
+#'
+#' These functions are defunct and no longer available.
+#' @rdname webchem-defunct
+#' @name webchem-defunct
+#' @param ... Parameters to be passed to the modern version of the function
+#' @docType package
+#' @export  allanwood
+#' @aliases allanwood
+#' @details Defunct functions are:
+#' \tabular{rl}{
+#'   \code{allanwood} \cr
+#' }
+allanwood <- function(...) {
+  .Defunct("alanwood", package = "webchem")
+}
