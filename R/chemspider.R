990600671fb89a76d9c806811f7d73aca5636f51
edisz <eduardszoecs@gmail.com>
2015-12-07 18:06:06 +0100

    cs_convert

--- R/chemspider.R
+++ R/chemspider.R
@@ -178,11 +178,58 @@ cs_extcompinfo <- function(csid, token, verbose = TRUE, ...){
 
 
 
-# cs_convert <- function(query, from = c('csid', 'inchi', 'inchikey', 'smiles', 'mol'),
-#                        to = c('csid', 'inchi',  'inchikey', 'smiles', 'mol'),
-#                        token) {
-#
-# }
+#' Convert identifiers using ChemSpider
+#'
+#' @param query character; query ID.
+#' @param from character; type of query ID.
+#' @param to character; type to convert to.
+#' @param token character; security token. Converting from csid to mol requires a token.
+#' @param verbose logical; should a verbose output be printed on the console?
+#' @param ... futher arguments passed. Currently onl \code{parse}, see also \code{\link{cs_csid_mol}}
+#' @return Depends on to. if \code{to = 'mol} then an RMol-Object, else a character string.
+#'
+#' @seealso \code{\link{parse_mol}} for a description of the Mol R Object.
+#' @note A security token is neeeded. Please register at RSC
+#' \url{https://www.rsc.org/rsc-id/register}
+#' for a security token.
+#' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
+#' @export
+#'
+#' @examples
+#' cs_convert('BQJCRHHNABKAKU-KBQPJGBKSA-N', from = 'inchikey', to = 'csid')
+#' cs_convert('BQJCRHHNABKAKU-KBQPJGBKSA-N', from = 'inchikey', to = 'inchi')
+#' cs_convert('BQJCRHHNABKAKU-KBQPJGBKSA-N', from = 'inchikey', to = 'mol')
+cs_convert <- function(query, from = c('csid', 'inchikey', 'inchi', 'smiles'),
+                       to = c('csid', 'inchikey', 'inchi', 'smiles', 'mol'),
+                       verbose = TRUE, token = NULL, ...) {
+  if (length(query) > 1) {
+    stop('Cannot handle multiple input strings.')
+  }
+  from <- match.arg(from)
+  to <- match.arg(to)
+  from_to <- paste(from, to , sep = '_')
+  if (from_to == 'csid_mol' & is.null(token)) {
+    stop('Need token for this conversion!')
+  }
+  # allowed combinations
+  comb <- c('csid_mol', 'inchikey_csid', 'inchikey_inchi', 'inchikey_mol',
+            'inchi_csid', 'inchi_inchikey', 'inchi_mol', 'inchi_smiles','smiles_inchi')
+  if (!from_to %in% comb) {
+    stop('Conversion from ', from, ' to ', to, ' currently not supported')
+  }
+  out <- switch(from_to,
+         csid_mol = cs_csid_mol(csid = query, token = token, verbose = verbose, ...),
+         inchikey_csid = cs_inchikey_csid(inchikey = query, verbose = verbose, ...),
+         inchikey_inchi = cs_inchikey_inchi(inchikey = query, verbose = verbose, ...),
+         inchikey_mol = cs_inchikey_mol(inchikey = query, verbose = verbose, ...),
+         inchi_csid = cs_inchi_csid(inchi = query, verbose = verbose, ...),
+         inchi_inchikey = cs_inchi_inchikey(inchi = query, verbose = verbose, ...),
+         inchi_mol = cs_inchi_mol(inchi = query, verbose = verbose, ...),
+         inchi_smiles = cs_inchi_smiles(inchi = query, verbose = verbose, ...),
+         smiles_inchi = cs_smiles_inchi(smiles = query, verbose = verbose, ...)
+         )
+  return(out)
+}
 
 
 
@@ -253,7 +300,7 @@ cs_csid_mol <- function(csid, token, parse = TRUE, verbose = TRUE, ...){
 #' @export
 #' @examples
 #' # convert CAS to CSID
-#' cs_inchikey_csid('BQJCRHHNABKAKU-KBQPJGBKSA-N', token = token)
+#' cs_inchikey_csid('BQJCRHHNABKAKU-KBQPJGBKSA-N')
 cs_inchikey_csid <- function(inchikey, verbose = TRUE, ...){
   # inchkey <- 'BQJCRHHNABKAKU-KBQPJGBKSA-N'
   if (length(inchikey) > 1) {
@@ -286,12 +333,7 @@ cs_inchikey_csid <- function(inchikey, verbose = TRUE, ...){
 #' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
 #' @export
 #' @examples
-#' \dontrun{
-#' # Fails because no TOKEN is included
-#' token <- '<YOUR-SECURITY-TOKEN>'
-#' # convert CAS to CSID
-#' cs_inchikey_inchi('BQJCRHHNABKAKU-KBQPJGBKSA-N', token = token)
-#' }
+#' cs_inchikey_inchi('BQJCRHHNABKAKU-KBQPJGBKSA-N')
 cs_inchikey_inchi <- function(inchikey, verbose = TRUE, ...){
   # inchikey <- 'BQJCRHHNABKAKU-KBQPJGBKSA-N'
   if (length(inchikey) > 1) {
@@ -329,9 +371,9 @@ cs_inchikey_inchi <- function(inchikey, verbose = TRUE, ...){
 #' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
 #' @export
 #' @examples
-#' tric_mol <- cs_inchikey_mol('BQJCRHHNABKAKU-KBQPJGBKSA-N', token = token)
+#' tric_mol <- cs_inchikey_mol('BQJCRHHNABKAKU-KBQPJGBKSA-N')
 #' tric_mol
-#' cs_inchikey_mol('BQJCRHHNABKAKU-KBQPJGBKSA-N', token = token, parse = FALSE)
+#' cs_inchikey_mol('BQJCRHHNABKAKU-KBQPJGBKSA-N',parse = FALSE)
 cs_inchikey_mol <- function(inchikey, parse = TRUE, verbose = TRUE, ...){
   # inchikey <- 'BQJCRHHNABKAKU-KBQPJGBKSA-N'
   if (length(inchikey) > 1) {
