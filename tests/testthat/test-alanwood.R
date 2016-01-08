11030f3c5066e63ce948f53b8980886d0f6b1f43
edisz <eduardszoecs@gmail.com>
2016-01-08 15:52:51 +0100

    fix for issue #96

--- tests/testthat/test-alanwood.R
+++ tests/testthat/test-alanwood.R
@@ -33,6 +33,10 @@ test_that("alanwood, cas", {
   cs <-  aw_query("79622-59-6", type = 'cas')
   xx <- aw_query('xxxxx', type = 'cas')
 
+  b1 <- aw_query('12071-83-9', type = 'cas')  # BUG: failed because of missing inchi / inchikey
+  expect_equal(b1$cas, '12071-83-9')
+  expect_equal(b1$inchikey, NA)
+
   expect_equal(cs$cas, "79622-59-6", verbose = FALSE)
   expect_equal(cs$cname, "fluazinam", verbose = FALSE)
   expect_equal(xx, NA, verbose = FALSE)
