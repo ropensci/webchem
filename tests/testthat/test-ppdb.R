f130f4eb4ee6439f0363363b89d14da2c2df5a9c
edisz <eduardszoecs@gmail.com>
2016-01-26 10:29:31 +0100

    ppdb source_url less tests

--- tests/testthat/test-ppdb.R
+++ tests/testthat/test-ppdb.R
@@ -12,23 +12,21 @@ test_that("ppdb_buildidx", {
 
 
 test_that("ppdb", {
-  y <- ppdb_query('1071-83-6')
-  z <- ppdb_query('50-00-0')
   xx <- ppdb_query('xxx')
 
-  b1 <- ppdb_query('553-82-2') # BUG: failed because of dups in ppdb_idx
-  b2 <- ppdb_query('1327-53-3') # BUG: failed because wrong encoding in website
+  y <- ppdb_query('553-82-2')
+  z <- ppdb_query('1327-53-3')
 
   expect_is(y, 'list')
   expect_is(z, 'list')
   expect_is(b1, 'list')
   expect_is(b2, 'list')
-  expect_equal(length(y), 10)
-  expect_equal(length(z), 10)
-  expect_equal(length(b1), 10)
-  expect_equal(length(b2), 10)
+  expect_equal(length(y), 11)
+  expect_equal(length(z), 11)
+  expect_equal(length(b1), 11)
+  expect_equal(length(b2), 11)
 
   expect_is(y$ec_regulation, 'data.frame')
-  expect_equal(y$general[y$general$variable == 'CAS RN', 'value'], "1071-83-6")
+  expect_equal(y$general[y$general$variable == 'CAS RN', 'value'], "553-82-2")
   expect_true(is.na(xx))
 })
