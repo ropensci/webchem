863a54fa67eb1aef45e4f92734cae0b83639b9dd
Eric R. Scott <scottericr@gmail.com>
2017-03-05 12:02:53 -0500

    Added roxygen2 header

--- R/flavornet.R
+++ R/flavornet.R
@@ -1,7 +1,25 @@
-# Retrieve flavor percepts from www.flavornet.org
-# 
-# Retreive flavor percepts from www.flavornet.org.  Flavornet is a database of 738 compounds with odors 
-# perceptible to humans detected using gas chromatography ofactometry (GCO).
+#' Retrieve flavor percepts from www.flavornet.org
+#' 
+#' Retreive flavor percepts from \url{http://www.flavornet.org}.  Flavornet is a database of 738 compounds with odors 
+#' perceptible to humans detected using gas chromatography ofactometry (GCO).
+#'
+#'@import xml2
+#'@importFrom stats rgamma
+#'
+#'@param CAS character; CAS number to search by. See \code{\link{is.cas}} for correct formatting
+#'@param verbose logical; should a verbose output be printed on the console?
+#'@return A named character vector containing flavor percepts or NA's in the case of CAS numbers that are not found
+#'
+#'@author Eric Scott, \email{eric.scott@@tufts.edu}
+#'
+#'@examples
+#'\donttest{
+#'fn_percept("123-32-0")
+#'
+#'CASs <- c("75-07-0",  "64-17-5",  "109-66-0", "78-94-4",  "78-93-3")
+#'fn_percept(CASs)
+#'}
+#'@export
 
 fn_percept <- function(CAS, verbose = TRUE, ...)
 {
