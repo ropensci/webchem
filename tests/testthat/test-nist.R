aa5b397ed61a596a1eb5deec8c16b1e2698409cd
edisz <eduardszoecs@gmail.com>
2018-06-24 22:25:39 +0200

    added tests for get_ri()

---
+++ tests/testthat/test-nist.R
@@ -0,0 +1,19 @@
+context("nist")
+
+a <- get_ri("78-70-6")
+b <- get_ri("78-70-6", type = 'linear', polarity = 'non-polar')
+c <- get_ri("78-70-6", type = 'linear', temp_prog = 'custom')
+
+test_that("nist returns correct results", {
+  skip_on_cran()
+
+  expect_is(a, 'data.frame')
+  expect_is(b, 'data.frame')
+  expect_is(c, 'data.frame')
+
+  expect_equal(names(a)[1], 'CAS')
+  expect_equal(names(b)[1], 'CAS')
+  expect_equal(names(c)[1], 'CAS')
+
+  expect_warning(get_ri("78-70-6", type = 'linear', polarity = 'non-polar'))
+})
