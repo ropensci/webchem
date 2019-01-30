7c782940027253904fb0d45d633281203b0073cc
morimoto <joshua.morimoto@tufts.edu>
2019-01-30 12:35:53 -0500

    Updated to be able to look for flavors in addition to odors

--- R/goodscents.R
+++ R/goodscents.R
@@ -2,16 +2,18 @@
 #'
 #' @import xml2
 #' @import rvest
-#' 
+#'
 #' @param CAS character; CAS number to search by. See \code{\link{is.cas}} for
 #' correct formatting.
-#' @param flavor boolean; FALSE by default, if TRUE, returns flavor instead of
-#' odor
-#' 
-#' @return Returns a character value of the found aromas or an NA if not found.
-#' 
+#' @param odor logical; TRUE by default, if TRUE, returns odor data from web
+#' @param flavor logical; FALSE by default, if TRUE, returns flavor data from web
+#' @param ... Currently not used
+#'
+#' @return Returns a list of character vectors the found odors and/or flavors
+#' or an NA if not found.
+#'
 #' @author Josh Morimoto, \email{joshua.morimoto@@tufts.edu}
-#' 
+#'
 #' @examples
 #' \donttest{
 #' tgsc_percept('66-25-1')
@@ -19,24 +21,40 @@
 #' @export
 
 
-tgsc_percept = function(CAS, flavor = FALSE) {
-  # Base url for goodscents
-  search.url = 'http://www.thegoodscentscompany.com/search3.php?qName='
-  if (!is.na(CAS)) {
-    # Paste CAS into url and read that page
-    stink.url = read_html(paste0(search.url, CAS)) #%>%
-    # Find the relevant data block in the page
-    stink.block = html_nodes(stink.url, '.lstw11') #%>%
-    # Read text
-    stink = html_text(stink.block)
-    if (flavor == TRUE) {
-    return(stink[2])
+
+tgsc_percept = function(CAS,
+                        odor = TRUE,
+                        flavor = FALSE,
+                        ...) {
+  foo = function(CAS, odor, flavor) {
+    # Base url for goodscents
+    search.url = 'http://www.thegoodscentscompany.com/search3.php?qName='
+    if (!is.na(CAS)) {
+      # Paste CAS into url and read that page
+      stink.url = read_html(paste0(search.url, CAS)) #%>%
+      # Find the relevant data block in the page
+      stink.block = html_nodes(stink.url, '.lstw11') #%>%
+      # Read text
+      stink = html_text(stink.block)
+      out = c(NULL, NULL)
+      # If odor checked, return first block
+      if (odor) {
+        out[1] = stink[1]
+      }
+      # If flavor checked, return second block
+      if (flavor) {
+        out[2] = stink[2]
+      }
+      # If only flavor, just return flavor element
+      if (!odor & flavor) {
+        out = out[2]
+      }
+      return(out)
     }
     else {
-      return(stink[1])
+      return(NA)
     }
   }
-  else {
-    return(NA)
-  }
+  out = lapply(CAS, foo, odor = odor, flavor = flavor)
+  return(out)
 }
