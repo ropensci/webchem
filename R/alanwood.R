bab8caea25253f29f1621bce4b5002a03a89f737
stitam <stirling.tamas@gmail.com>
2020-04-03 22:46:49 +0100

    build_aw_idx to tempdir(), remove aw_idx from package

--- R/alanwood.R
+++ R/alanwood.R
@@ -8,9 +8,8 @@
 #' @param  query character; search string
 #' @param type character; type of input ('cas' or 'commonname')
 #' @param verbose logical; print message during processing to console?
-#' @param idx data.frame; index to use. If NULL (default) the internal index
-#' \code{\link{aw_idx}} is used. To rebuild the index use
-#' \code{\link{build_aw_idx}}.
+#' @param force_build logical; force building a new index? See
+#' \code{build_aw_idx()} for more details.
 #' @return A list of eight entries: common-name, status, preferred IUPAC Name,
 #' IUPAC Name, cas, formula, activity, subactivity, inchikey, inchi and source
 #' url.
@@ -31,24 +30,22 @@
 #' aw_query("79622-59-6", type = 'cas')
 #' }
 aw_query <- function(query, type = c("commonname", "cas"), verbose = TRUE,
-                     idx = NULL) {
-  foo <- function(query, type = c("commonname", "cas"), verbose, idx = NULL) {
+                     force_build = FALSE) {
+  aw_idx <- suppressMessages(build_aw_idx(verbose, force_build))
+  foo <- function(query, type = c("commonname", "cas"), verbose) {
     type <- match.arg(type)
-    if (is.null(idx)) {
-      idx <- webchem::aw_idx
-    }
   # search links in indexes
     if (type == "commonname") {
-      links <- idx$links[idx$source == "cn"]
-      names <- idx$linknames[idx$source == "cn"]
+      links <- aw_idx$links[aw_idx$source == "cn"]
+      names <- aw_idx$linknames[aw_idx$source == "cn"]
       cname <-  query
     }
 
     if (type == "cas") {
-      names <- idx$names[idx$source == "rn"]
+      names <- aw_idx$names[aw_idx$source == "rn"]
       # select only first link
-      links <- idx$links[idx$source == "rn"]
-      linknames <- idx$linknames[idx$source == "rn"]
+      links <- aw_idx$links[aw_idx$source == "rn"]
+      linknames <- aw_idx$linknames[aw_idx$source == "rn"]
       cname <-  linknames[tolower(names) == tolower(query)]
     }
 
@@ -128,7 +125,7 @@ aw_query <- function(query, type = c("commonname", "cas"), verbose = TRUE,
                 source_url = source_url)
     return(out)
   }
-  out <- lapply(query, foo, type = type, verbose = verbose, idx = idx)
+  out <- lapply(query, function(x) foo(x, type = type, verbose = verbose))
   out <- setNames(out, query)
   class(out) <- c("aw_query", "list")
   return(out)
@@ -137,42 +134,61 @@ aw_query <- function(query, type = c("commonname", "cas"), verbose = TRUE,
 
 #' Function to build index
 #'
+#' This function builds an index of Alan Woods Compendium of Pesticides
+#' \url{http://www.alanwood.net/pesticides} and saves it to \code{tempdir()}.
+#' This is a utility function for \code{aw_query()} and will not be exported in
+#' future releases.
 #' @import xml2
+#' @param verbose logical; print message during processing to console?
+#' @param force_build logical; force building a new index?
 #' @return a data.frame with three columns: cas, url, and name
-#' @export
-#' @seealso \code{\link{aw_query}}, \code{\link{aw_idx}}
-#'@author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
-build_aw_idx <- function() {
-  idx1 <- read_html("http://www.alanwood.net/pesticides/index_rn.html")
-  prep_idx <- function(y) {
-    names <- xml_text(xml_find_all(y, "//dl/dt"))
-    links <- xml_attr(
-      xml_find_all(y, "//dt/following-sibling::dd[1]/a[1]"), "href")
-    linknames <- xml_text(xml_find_all(y, "//dt/following-sibling::dd[1]/a[1]"))
-    return(data.frame(names, links, linknames, stringsAsFactors = FALSE))
+#' @seealso \code{\link{aw_query}}
+#' @author Eduard Szoecs, \email{eduardszoecs@@gmail.com}
+#' @source \url{http://www.alanwood.net/pesticides}
+build_aw_idx <- function(verbose = TRUE, force_build = FALSE) {
+  message(msg = "build_aw_idx() will not be exported in future releases.")
+  suppressWarnings(try(load(paste0(tempdir(), "/data/aw_idx.rda")),
+                       silent = TRUE))
+  if (!file.exists(paste0(tempdir(), "/data/aw_idx.rda")) |
+      force_build == TRUE |
+      Sys.Date() - attr(aw_idx, "date") > 30) {
+    if (!dir.exists(paste0(tempdir(), "/data"))) {
+      dir.create(paste0(tempdir(), "/data"))
+    }
+    if (verbose == TRUE) {
+      message("Building index.", appendLF = FALSE)
+    }
+    idx1 <- read_html("http://www.alanwood.net/pesticides/index_rn.html")
+    prep_idx <- function(y) {
+      names <- xml_text(xml_find_all(y, "//dl/dt"))
+      links <- xml_attr(
+        xml_find_all(y, "//dt/following-sibling::dd[1]/a[1]"), "href")
+      linknames <- xml_text(xml_find_all(y, "//dt/following-sibling::dd[1]/a[1]"))
+      return(data.frame(names, links, linknames, stringsAsFactors = FALSE))
+    }
+    aw_idx <- rbind(prep_idx(idx1))
+    aw_idx[["source"]] <- "rn"
+    idx4 <- read_html("http://www.alanwood.net/pesticides/index_cn.html")
+    n <- xml_find_all(idx4, "//a")
+    names <- xml_text(n)
+    rm <- names == ""
+    names <- names[!rm]
+    links <- xml_attr(n, "href")
+    links <- links[!rm]
+    idx4 <- data.frame(names = NA, links = links, linknames = names,
+                       source = "cn", stringsAsFactors = FALSE)
+    aw_idx <- rbind(aw_idx, idx4)
+
+    # fix encoding
+    ln <- aw_idx$linknames
+    Encoding(ln) <- "latin1"
+    ln <- iconv(ln, from = "latin1", to = "ASCII", sub = "")
+    aw_idx$linknames <- ln
+    attr(aw_idx, "date") <- Sys.Date()
+    if (verbose == TRUE) {
+      message(" Done.")
+    }
+    save(aw_idx, file = paste0(tempdir(), "/data/aw_idx.rda"))
   }
-  aw_idx <- rbind(prep_idx(idx1))
-  aw_idx[["source"]] <- "rn"
-  idx4 <- read_html("http://www.alanwood.net/pesticides/index_cn.html")
-  n <- xml_find_all(idx4, "//a")
-  names <- xml_text(n)
-  rm <- names == ""
-  names <- names[!rm]
-  links <- xml_attr(n, "href")
-  links <- links[!rm]
-  idx4 <- data.frame(names = NA, links = links, linknames = names,
-                     source = "cn", stringsAsFactors = FALSE)
-  aw_idx <- rbind(aw_idx, idx4)
-
-  # fix encoding
-  ln <- aw_idx$linknames
-  Encoding(ln) <- "latin1"
-  ln <- iconv(ln, from = "latin1", to = "ASCII", sub = "")
-  aw_idx$linknames <- ln
-  attr(aw_idx, "date") <- Sys.Date()
-
-  # do not delete this line
-  # occasionally the data file should be updated in the package
-  # save(aw_idx, file = "data/aw_idx.rda")
   return(aw_idx)
 }
