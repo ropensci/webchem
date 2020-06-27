e29b47618a053129c68bf77fc1e65386330f9398
Aariq <scottericr@gmail.com>
2020-06-27 14:44:27 -0400

    add skips for integration function tests.

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -1,8 +1,13 @@
 # These all might occasionally fail because cts_translate() is currently somewhat unreliable.
 
 fn_up <- ping_service("fn")
+up <- ping_service("cts")
+
 test_that("autotranslate works when no translation needed", {
+  skip_on_cran()
   skip_if_not(fn_up, "Flavornet down!")
+  skip_if_not(up, "CTS service down")
+
   CASs <- c("75-07-0",  "64-17-5")
   a <- autotranslate(query = CASs, from = "cas", .f = "fn_percept", .verbose = TRUE)
   b <- fn_percept(CASs)
@@ -11,7 +16,10 @@ test_that("autotranslate works when no translation needed", {
 
 etox_up <- ping_service("etox")
 test_that("autotranslate translates", {
+  skip_on_cran()
   skip_if_not(etox_up, "ETOX down!")
+  skip_if_not(up, "CTS service down")
+
   x <- autotranslate(query = "XDDAORKBJWWYJS-UHFFFAOYSA-N", from = "inchikey", .f = "get_etoxid")
   y <- get_etoxid(query = "1071-83-6", from = "cas")
   expect_equal(x, y)
@@ -19,8 +27,11 @@ test_that("autotranslate translates", {
 
 
 test_that("has_entry() function works", {
+  skip_on_cran()
   skip_if_not(fn_up)
   skip_if_not(etox_up)
+  skip_if_not(up, "CTS service down")
+
   out <- has_entry(c("triclosan", NA, "balloon"),
                         from = "name",
                         sources = c("etox", "fn"))
