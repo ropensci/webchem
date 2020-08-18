d88c1fce565195ab471b783044a3fc24d6a0f778
stitam <stirling.tamas@gmail.com>
2020-08-18 22:44:10 +0100

    RETRY and verbose messages for flavornet

--- R/flavornet.R
+++ R/flavornet.R
@@ -5,7 +5,7 @@
 #'
 #' @import xml2
 #' @importFrom stats rgamma
-#'
+#' @importFrom httr RETRY message_for_status
 #' @param query character; CAS number to search by. See \code{\link{is.cas}} for correct formatting
 #' @param from character; currently only CAS numbers are accepted.
 #' @param verbose logical; should a verbose output be printed on the console?
@@ -34,21 +34,26 @@ fn_percept <- function(query, from = "cas", verbose = TRUE, CAS, ...)
   }
   match.arg(from)
   foo <- function (query, verbose){
-    if (is.na(query)) return(NA)
-    on.exit(suppressWarnings(closeAllConnections()))
-    qurl <- paste0("http://www.flavornet.org/info/",query,".html")
-    if (verbose)
-      message(qurl)
-    Sys.sleep(rgamma(1, shape = 10, scale = 1/10))
-    h <- try(read_html(qurl), silent = TRUE)
-    if (inherits(h, "try-error")) {
-      warning("CAS not found... Returning NA.")
+    if (is.na(query)) {
+      if (verbose) message("Query is NA. Returning NA.")
       return(NA)
     }
-    doc.text = xml_text(xml_find_all(h, "/html/body/p[3]"))
-    pattern = "Percepts: "
-    percept <- gsub(pattern, "", doc.text)
-    return(percept)
+    qurl <- paste0("http://www.flavornet.org/info/",query,".html")
+    if(verbose) message(paste0("Querying ", query, ". "), appendLF = FALSE)
+    Sys.sleep(stats::rgamma(1, shape = 10, scale = 1/10))
+    h <- httr::RETRY("GET", qurl, terminate_on = 404, quiet = TRUE)
+    if (h$status_code == 200){
+      if (verbose) message(httr::message_for_status(h))
+      h <- read_html(qurl)
+      doc.text = xml_text(xml_find_all(h, "/html/body/p[3]"))
+      pattern = "Percepts: "
+      percept <- gsub(pattern, "", doc.text)
+      return(percept)
+    }
+    else {
+      if (verbose) message(httr::message_for_status(h))
+      return(NA)
+    }
   }
   percepts <- sapply(query, foo, verbose = verbose)
   percepts <- setNames(percepts, query)
