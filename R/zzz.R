c4c3a941e650f05758be0b466d85ef43c8511f7a
stitam <stirling.tamas@gmail.com>
2021-01-28 20:28:01 +0100

    Remove startup message pointing to the 2020 survey

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Help us improve the package")
-  packageStartupMessage("Fill out our 2020 survey at https://forms.gle/V7dfGGn73dkesn5L6")
-}
