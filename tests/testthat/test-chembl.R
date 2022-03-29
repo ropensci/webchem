8265d665dfae16d3fb188d28784cb66c34a69410
Tamas Stirling <stirling.tamas@gmail.com>
2022-03-29 11:05:21 +0200

    Add more tests based on codecov

--- tests/testthat/test-chembl.R
+++ tests/testthat/test-chembl.R
@@ -6,14 +6,55 @@ test_that("chembl_query()", {
 
   #examples
   o1 <- chembl_query("CHEMBL1082", resource = "molecule")
+  o1m <- capture_messages(
+    chembl_query("CHEMBL1082", resource = "molecule", verbose = TRUE))
+
   o2 <- chembl_query(c("CHEMBL25", "CHEMBL1082"), resource = "molecule")
   o3 <- chembl_query("CHEMBL771355", "chembl_id_lookup")
   o4 <- chembl_query("CHEMBL771355", resource = "assay")
 
   expect_type(o1, "list")
+  expect_equal(length(o1[[1]]), 38)
+  expect_equal(o1m[2], "OK (HTTP 200).")
+  expect_equal(length(o2), 2)
+  expect_equal(o3[[1]]$entity_type, "ASSAY")
+  expect_equal(o4[[1]]$document_chembl_id, "CHEMBL1121620")
 
   #invalid inputs
-  o5 <- chembl_query("CHEMBL1082", NA, "pumpkin", "CHEMBL25")
+  o5 <- chembl_query(c("CHEMBL1082", NA, "pumpkin", "CHEMBL25"))
+  o5m <- capture_messages(
+    chembl_query(c("CHEMBL1082", NA, "pumpkin", "CHEMBL25"), verbose = TRUE)
+  )
 
+  expect_equal(length(o5), 4)
+  expect_equal(o5m[4], capture_messages(webchem_message("na"))[1])
+  expect_equal(o5m[6], "Query is not a ChEMBL ID. Returning NA.\n")
 
+  #caching
+  o6 <- chembl_query("CHEMBL1082", resource = "molecule", cache = TRUE)
+  o7m <- capture_messages(chembl_query("CHEMBL1082", resource = "molecule",
+                                       cache = TRUE, verbose = TRUE)
+  )
+  o8 <- chembl_query(NA, resource = "molecule", cache = TRUE)
+
+  expect_equal(o7m[1], "Querying CHEMBL1082. ")
+  expect_equal(o7m[2], "Already retrieved.\n")
+  expect_equal(o8[[1]], NA)
+
+  if (file.exists("query_results.rda")) file.remove("query_results.rda")
+
+  #messages
+  o9 <- capture_messages(
+    chembl_query("CHEMBL12345678", resource = "molecule", verbose = TRUE))
+
+  expect_equal(o9[2], "Not Found (HTTP 404).")
+
+})
+
+test_that("atc_classes()", {
+  o1 <- atc_classes()
+  o2 <- capture_messages(atc_classes(verbose = TRUE))
+
+  expect_s3_class(o1, c("tbl_df", "tbl", "data.frame"))
+  expect_equal(o2[1], "OK (HTTP 200).")
 })
