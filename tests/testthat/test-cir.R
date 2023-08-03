968a96427537e2183005e4f275cd2a06dfe9c509
Eric R. Scott <scottericr@gmail.com>
2023-08-03 15:58:50 -0400

    vcr cts

--- tests/testthat/test-cir.R
+++ tests/testthat/test-cir.R
@@ -1,26 +1,23 @@
 up <- ping_service("cir")
 test_that("cir_query()", {
-  # skip_on_cran()
-  # skip_if_not(up, "CIR server is down")
-  vcr::use_cassette("cir_query", {
-    expect_equal(cir_query('Triclosan', 'mw')$mw[1], 289.5451)
-    expect_equal(cir_query('xxxxxxx', 'mw')$mw[1], NA)
-    expect_equal(cir_query("3380-34-5", 'stdinchikey', resolver = 'cas_number')$stdinchikey[1],
-                 "InChIKey=XEFQLINVKFYRCS-UHFFFAOYSA-N")
-    expect_true(nrow(cir_query('Triclosan', 'cas')) > 1)
-    expect_true(nrow(cir_query('Triclosan', 'cas', match = "first")) == 1)
-    expect_true(nrow(cir_query(c('Triclosan', 'Aspirin'), 'cas', match = "first")) == 2)
+  skip_on_cran()
+  skip_if_not(up, "CIR server is down")
+  expect_equal(cir_query('Triclosan', 'mw')$mw[1], 289.5451)
+  expect_equal(cir_query('xxxxxxx', 'mw')$mw[1], NA)
+  expect_equal(cir_query("3380-34-5", 'stdinchikey', resolver = 'cas_number')$stdinchikey[1],
+               "InChIKey=XEFQLINVKFYRCS-UHFFFAOYSA-N")
+  expect_true(nrow(cir_query('Triclosan', 'cas')) > 1)
+  expect_true(nrow(cir_query('Triclosan', 'cas', match = "first")) == 1)
+  expect_true(nrow(cir_query(c('Triclosan', 'Aspirin'), 'cas', match = "first")) == 2)
 
-    expect_equal(cir_query('acetic acid', 'mw', match = "first"),
-                 tibble(query = "acetic acid", mw = 60.0524))
-  })
+  expect_equal(cir_query('acetic acid', 'mw', match = "first"),
+               tibble(query = "acetic acid", mw = 60.0524))
 
 })
 
 test_that("cir_query() handles special characters in SMILES", {
   skip_on_cran()
   skip_if_not(up, "CIR server is down")
-
   expect_equal(cir_query("C#C", representation = "inchikey")$inchikey,
                "InChIKey=HSFWRNGVRCDJHI-UHFFFAOYNA-N")
 })
@@ -28,7 +25,6 @@ test_that("cir_query() handles special characters in SMILES", {
 test_that("cir_query() handles NA queries and queries that return NA", {
   skip_on_cran()
   skip_if_not(up, "CIR server is down")
-
   expect_identical(
     cir_query(c("Triclosan", "pumpkin", NA), representation = "cas",match = "first"),
     tibble(query = c("Triclosan", "pumpkin", NA_character_),
