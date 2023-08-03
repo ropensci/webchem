054a6e912e7870025cf19dc0b819503444294a01
Eric R. Scott <scottericr@gmail.com>
2023-08-03 13:25:17 -0400

    cts

---
+++ tests/testthat/mocks/cts_compinfo/cts.fiehnlab.ucdavis.edu/service/compound/XEFQLINVKFYRCS-UHFFFAOYSA-X.R
@@ -0,0 +1,16 @@
+structure(list(url = "http://cts.fiehnlab.ucdavis.edu/service/compound/XEFQLINVKFYRCS-UHFFFAOYSA-X", 
+    status_code = 400L, headers = structure(list(date = "Thu, 03 Aug 2023 17:22:07 GMT", 
+        `content-type` = "application/json;charset=utf-8", `transfer-encoding` = "chunked"), class = c("insensitive", 
+    "list")), all_headers = list(list(status = 400L, version = "HTTP/1.1", 
+        headers = structure(list(date = "Thu, 03 Aug 2023 17:22:07 GMT", 
+            `content-type` = "application/json;charset=utf-8", 
+            `transfer-encoding` = "chunked"), class = c("insensitive", 
+        "list")))), cookies = structure(list(domain = logical(0), 
+        flag = logical(0), path = logical(0), secure = logical(0), 
+        expiration = structure(numeric(0), class = c("POSIXct", 
+        "POSIXt")), name = logical(0), value = logical(0)), row.names = integer(0), class = "data.frame"), 
+    content = charToRaw("\n[\n  \"Sorry, we couldn't find any matching results\"\n]"), 
+    date = structure(1691083327, class = c("POSIXct", "POSIXt"
+    ), tzone = "GMT"), times = c(redirect = 0, namelookup = 4.6e-05, 
+    connect = 0, pretransfer = 0.000188, starttransfer = 0.126594, 
+    total = 0.126627)), class = "response")
