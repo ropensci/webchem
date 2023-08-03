2add1efc29172a3971d841618fd3e5541de82e24
Eric R. Scott <scottericr@gmail.com>
2023-08-03 15:52:40 -0400

    remove redundant test

--- tests/testthat/test-cir.R
+++ tests/testthat/test-cir.R
@@ -1,26 +1,20 @@
 up <- ping_service("cir")
 test_that("cir_query()", {
-  skip_on_cran()
-  skip_if_not(up, "CIR server is down")
+  # skip_on_cran()
+  # skip_if_not(up, "CIR server is down")
+  vcr::use_cassette("cir_query", {
+    expect_equal(cir_query('Triclosan', 'mw')$mw[1], 289.5451)
+    expect_equal(cir_query('xxxxxxx', 'mw')$mw[1], NA)
+    expect_equal(cir_query("3380-34-5", 'stdinchikey', resolver = 'cas_number')$stdinchikey[1],
+                 "InChIKey=XEFQLINVKFYRCS-UHFFFAOYSA-N")
+    expect_true(nrow(cir_query('Triclosan', 'cas')) > 1)
+    expect_true(nrow(cir_query('Triclosan', 'cas', match = "first")) == 1)
+    expect_true(nrow(cir_query(c('Triclosan', 'Aspirin'), 'cas', match = "first")) == 2)
 
-  expect_equal(cir_query('Triclosan', 'mw')$mw[1], 289.5451)
-  expect_equal(cir_query('xxxxxxx', 'mw')$mw[1], NA)
-  expect_equal(cir_query("3380-34-5", 'stdinchikey', resolver = 'cas_number')$stdinchikey[1],
-            "InChIKey=XEFQLINVKFYRCS-UHFFFAOYSA-N")
-  expect_true(nrow(cir_query('Triclosan', 'cas')) > 1)
-  expect_true(nrow(cir_query('Triclosan', 'cas', match = "first")) == 1)
-  expect_true(nrow(cir_query(c('Triclosan', 'Aspirin'), 'cas', match = "first")) == 2)
+    expect_equal(cir_query('acetic acid', 'mw', match = "first"),
+                 tibble(query = "acetic acid", mw = 60.0524))
+  })
 
-  expect_equal(cir_query('acetic acid', 'mw', match = "first"),
-               tibble(query = "acetic acid", mw = 60.0524))
-
-})
-
-test_that("cir_query() doesn't mistake NA for sodium", {
-  skip_on_cran()
-  skip_if_not(up, "CIR server is down")
-
-  expect_true(is.na(cir_query(as.character(NA), 'cas')$cas))
 })
 
 test_that("cir_query() handles special characters in SMILES", {
